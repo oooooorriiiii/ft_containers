729b71ea2ced8a1a67a856b709f7f0b2b3dc3389
ymori <ymori@student.42tokyo.jp>
2023-02-03 14:51:15 +0900

    add: ft_algorithm.hpp

--- includes/ft_algorithm.hpp
+++ includes/ft_algorithm.hpp
@@ -11,9 +11,9 @@ namespace ft {
  * equal
  */
 
-template <class InputIter1, class InputIter2>
+template<class InputIter1, class InputIter2>
 bool equal(InputIter1 first1, InputIter1 last1, InputIter2 first2) {
-  for ( ; first1 != last1; ++first1, ++first2) {
+  for (; first1 != last1; ++first1, ++first2) {
     if (!(bool(*first1 == *first2))) {
       return false;
     }
@@ -21,9 +21,12 @@ bool equal(InputIter1 first1, InputIter1 last1, InputIter2 first2) {
   return true;
 }
 
-template <class InputIter1, class InputIter2, class BinaryPredicate>
-bool equal(InputIter1 first1, InputIter1 last1, InputIter2 first2, BinaryPredicate p) {
-  for ( ; first1 != last1; ++first1, ++first2) {
+template<class InputIter1, class InputIter2, class BinaryPredicate>
+bool equal(InputIter1 first1,
+           InputIter1 last1,
+           InputIter2 first2,
+           BinaryPredicate p) {
+  for (; first1 != last1; ++first1, ++first2) {
     if (!(bool(p(*first1, *first2)))) {
       return false;
     }
@@ -35,9 +38,12 @@ bool equal(InputIter1 first1, InputIter1 last1, InputIter2 first2, BinaryPredica
  * lexicographical_compare
  */
 
-template <class InputIter1, class InputIter2>
-bool lexicographical_compare(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2) {
-  for ( ; (first1 != last1) && (first2 != last2); ++first1, (void)++first2) {
+template<class InputIter1, class InputIter2>
+bool lexicographical_compare(InputIter1 first1,
+                             InputIter1 last1,
+                             InputIter2 first2,
+                             InputIter2 last2) {
+  for (; (first1 != last1) && (first2 != last2); ++first1, (void)++first2) {
     if (*first1 < *first2) {
       return true;
     }
@@ -48,9 +54,13 @@ bool lexicographical_compare(InputIter1 first1, InputIter1 last1, InputIter2 fir
   }
 }
 
-template <class InputIter1, class InputIter2, class Compare>
-bool lexicographical_compare(InputIter1 first1, InputIter1 last1, InputIter2 first2, InputIter2 last2, Compare comp) {
-  for ( ; (first1 != last1) && (first2 != last2); ++first1, (void)++first2) {
+template<class InputIter1, class InputIter2, class Compare>
+bool lexicographical_compare(InputIter1 first1,
+                             InputIter1 last1,
+                             InputIter2 first2,
+                             InputIter2 last2,
+                             Compare comp) {
+  for (; (first1 != last1) && (first2 != last2); ++first1, (void)++first2) {
     if (comp(*first1, *first2)) {
       return true;
     }
